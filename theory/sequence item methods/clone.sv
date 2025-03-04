ada0def6b3872d6a507859673e7484b1ab8a4736
Daksh Hanswal <dakshhanswal@gmail.com>
2025-03-04 12:31:11 +0530

    Update clone.sv

--- theory/sequence item methods/clone.sv
+++ theory/sequence item methods/clone.sv
@@ -1,3 +1,6 @@
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
 class seq_item extends uvm_sequence_item;
   
   rand bit [3:0] addr;
@@ -29,9 +32,12 @@ module tb;
   initial
     begin
       inst1 = seq_item::type_id::create();
+      inst1.randomize();
+      `uvm_info("original", "printing inst1", UVM_LOW);
+      inst1.print();
 
       $cast(inst3, inst1.clone());
-
+      `uvm_info("cloned", "printing inst3", UVM_LOW);
       inst3.print();
     end
   
